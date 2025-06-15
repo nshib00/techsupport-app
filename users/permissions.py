a1339fb4e5139ddff355501d70e6f3f245235533
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-15 14:42:18 +0300

    Create custom IsAdmin permission, update IsSupport permission

--- users/permissions.py
+++ users/permissions.py
@@ -1,6 +1,18 @@
 from rest_framework.permissions import BasePermission
+from users.models import User
+
 
 
 class IsSupportUser(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'support'
+        if isinstance(request.user, User):
+            return request.user.role == 'support' and request.user.is_active
+        return False
+    
+
+class IsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        if isinstance(request.user, User):
+            return request.user.role == 'admin' and request.user.is_active
+        return False
+    
