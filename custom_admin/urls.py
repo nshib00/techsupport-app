485113c69d1a7199dcc1b90077e3fc0457dadfb9
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-15 17:30:24 +0300

    Add admin methods for getting users with role filter

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from custom_admin.views.tickets import TicketAssignView, TicketUpdateStatusView
 from custom_admin.views.ticket_categories import TicketCategoryCreateView
+from custom_admin.views.users import UserListView
 
 
 urlpatterns = [
+    # Тикеты
     path('tickets/<int:pk>/status/', TicketUpdateStatusView.as_view(), name='ticket-status-update'), 
     path('tickets/<int:pk>/assign/', TicketAssignView.as_view(), name='ticket-assign'),
     path('tickets/categories/', TicketCategoryCreateView.as_view(), name='ticket-categories'),
+
+    # Пользователи
+    path('users/', UserListView.as_view(), name='users-list'),
 ]
---
+++ custom_admin/views/users.py
@@ -0,0 +1,32 @@
+from rest_framework.generics import ListAPIView
+from users.models import User
+from users.serializers import UserSerializer
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse
+from rest_framework.exceptions import ValidationError
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name='role',
+            description='Фильтрация пользователей по роли (список ролей: admin, user, support)',
+            required=False,
+            type=str
+        )
+    ],
+    responses={
+        200: UserSerializer(many=True),
+        400: OpenApiResponse(description='Некорректный параметр запроса'),
+        500: OpenApiResponse(description='Внутренняя ошибка сервера'),
+    }
+)
+class UserListView(ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        role = self.request.GET.get('role')
+        if role not in User.ROLE_CHOICES:
+            raise ValidationError({'role': 'Недопустимое значение роли.'})
+        if role is not None:
+            return User.objects.filter(role=role)
+        return User.objects.all()
