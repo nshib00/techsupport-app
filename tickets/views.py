b513444d3ad31c578f67aa77891d21506e7e16e2
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-15 23:55:08 +0300

    Implement creating tickets

--- tickets/views.py
+++ tickets/views.py
@@ -1,19 +1,43 @@
-from rest_framework import generics
+from rest_framework import generics, serializers
 from rest_framework.permissions import IsAuthenticated
 from tickets.models.ticket import Ticket
 from tickets.models.ticket_attachment import TicketAttachment
 from tickets.models.ticket_comment import TicketComment
-from tickets.serializers.tickets import TicketListRetrieveSerializer, TicketSerializer, TicketStatusSerializer
+from tickets.serializers.tickets import TicketListRetrieveSerializer, TicketSerializer
 from tickets.serializers.ticket_comments import TicketCommentSerializer
-from drf_spectacular.utils import extend_schema_view, extend_schema
+from drf_spectacular.utils import extend_schema_view, extend_schema, inline_serializer, OpenApiResponse
 
 
 @extend_schema_view(
     get=extend_schema(
         summary="Список тикетов, созданных пользователем",
+        responses={
+            200: TicketSerializer(many=True),
+            401: OpenApiResponse(description="Пользователь не авторизован")
+        }
     ),
     post=extend_schema(
         summary="Создание нового тикета",
+        request={
+            'multipart/form-data': inline_serializer(
+                name='TicketCreateRequest',
+                fields={
+                    'subject': serializers.CharField(),
+                    'description': serializers.CharField(),
+                    'category': serializers.IntegerField(),
+                    'attachments': serializers.ListField(
+                        child=serializers.FileField(),
+                        required=False,
+                        help_text="Файлы вложений (можно загрузить несколько)"
+                    ),
+                }
+            )
+        },
+        responses={
+            201: TicketSerializer,
+            400: OpenApiResponse(description="Неверные данные"),
+            401: OpenApiResponse(description="Пользователь не авторизован")
+        }
     )
 )
 class TicketListCreateView(generics.ListCreateAPIView):
@@ -25,9 +49,9 @@ class TicketListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         attachments = self.request.FILES.getlist('attachments')
-        ticket = serializer.save()
+        ticket = serializer.save(user=self.request.user)
         for file in attachments:
-            TicketAttachment.objects.create(ticket=ticket, file=file)
+            TicketAttachment.objects.create(ticket=ticket, user=self.request.user, file=file)
 
 
 @extend_schema_view(
@@ -63,3 +87,6 @@ class TicketCommentView(generics.ListCreateAPIView):
             user=self.request.user,
             ticket_id=self.kwargs['ticket_id']
         )
+
+
+# class Ticket
