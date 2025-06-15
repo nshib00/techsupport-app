db66c4e18a65604541cd51bd4f8b1414aea4ba60
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-15 23:52:36 +0300

    Fix get and post responses in TicketCategoryCreateView

--- custom_admin/views/ticket_categories.py
+++ custom_admin/views/ticket_categories.py
@@ -9,15 +9,20 @@ from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiResp
 @extend_schema_view(
     get=extend_schema(
         summary="Список всех категорий тикетов",
+        responses={
+            200: TicketCategorySerializer,
+            401: OpenApiResponse(description="Пользователь не авторизован"),
+            403: OpenApiResponse(description="Нет прав для работы с категориями"),
+        }
     ),
     post=extend_schema(
         summary="Создание категории тикетов",
+        responses={
+            201: TicketCategorySerializer,
+            401: OpenApiResponse(description="Пользователь не авторизован"),
+            403: OpenApiResponse(description="Нет прав для работы с категориями"),
+        }
     ),
-    responses={
-        200: TicketCategorySerializer,
-        401: OpenApiResponse(description="Пользователь не авторизован"),
-        403: OpenApiResponse(description="Нет прав для работы с категориями"),
-    }
 )
 class TicketCategoryCreateView(ListCreateAPIView):
     serializer_class = TicketCategorySerializer
