5f610f6212a669ce3d3836e9e289798730e53e1d
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-15 14:22:36 +0300

    Change URL for ticket status update method: tickets/update_status/ -> tickets/

--- tickets/urls.py
+++ tickets/urls.py
@@ -7,7 +7,7 @@ from tickets.views import (
 urlpatterns = [
     path('', TicketListCreateView.as_view(), name='tickets-list-create'),
     path('<int:pk>/', UserTicketsRetrieveView.as_view(), name='ticket-detail'), 
-    path('<int:pk>/update_status/', TicketUpdateStatusView.as_view(), name='ticket-status-update'), 
+    path('<int:pk>/', TicketUpdateStatusView.as_view(), name='ticket-status-update'), 
     path('<int:pk>/comments', TicketCommentView.as_view(), name='ticket-comments'),
     path('<int:pk>/assign', TicketAssignView.as_view(), name='ticket-assign'),
 ] 
