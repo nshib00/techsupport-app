762ccf53aa2b79c8511128fa736022d60e618773
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-18 15:38:12 +0300

    Update TicketCategory model

--- tickets/models/ticket_category.py
+++ tickets/models/ticket_category.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class TicketCategory(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название категории')
-    description = models.TextField(blank=True, verbose_name='Описание')
-    is_active = models.BooleanField(default=True, verbose_name='Активна ли категория')
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название категории',
+        unique=True,
+        validators=[MinLengthValidator(3)]
+    )
+    description = models.TextField(blank=True, max_length=500, verbose_name='Описание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    
+
     class Meta:
         verbose_name = 'Категория обращения'
         verbose_name_plural = 'Категории обращений'
