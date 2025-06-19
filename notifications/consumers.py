1f0bca91277ac1f92386d5ae0be9470b4da800ce
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-19 03:57:41 +0300

    Implement ticket create notifications for support

---
+++ notifications/consumers.py
@@ -0,0 +1,39 @@
+import json
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
+from users.models import User
+
+
+class SupportNotificationConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        user = self.scope['user']
+        if user.is_authenticated and await self.is_support(user):
+            await self.channel_layer.group_add("support", self.channel_name)
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard("support", self.channel_name)
+
+    async def send_json(self, content, close=False):
+        # переопределение метода send_json для передачи кириллицы без unicode-кодирования
+        await self.send(
+            text_data=json.dumps(content, ensure_ascii=False),
+            close=close
+        )
+
+    async def notify_new_ticket(self, event):
+        await self.send_json({
+            'type': 'new_ticket',
+            'ticket_id': event['ticket_id'],
+            'subject': event['subject'],
+            'category': event['category'],
+            'user': event['user'],
+            'created_at': event['created_at'],
+            'link': event['link'],
+        })
+
+    @database_sync_to_async
+    def is_support(self, user: User):
+        return user.is_support()
---
+++ tickets/signals.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Ticket
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+
+@receiver(post_save, sender=Ticket)
+def notify_support_on_new_ticket(sender, instance, created, **kwargs):
+    if created:
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            "support",
+            {
+                "type": "notify_new_ticket",
+                "ticket_id": instance.id,
+                "subject": instance.subject,
+                "category": instance.category.name if instance.category else None,
+                "user": {
+                    "id": instance.user.id,
+                    "email": instance.user.email,
+                },
+                "created_at": instance.created_at.isoformat(),
+                "link": f"/support/tickets/{instance.id}/",
+            }
+        )
