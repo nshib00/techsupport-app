70f6b36f441d84b1dd1874654181d192f1eb9e60
Nikita Shibaev <nshib.54321@gmail.com>
2025-07-02 12:42:59 +0300

    Change cors url in dev settings

--- techsupport/settings/dev.py
+++ techsupport/settings/dev.py
@@ -4,11 +4,11 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 DATABASES = {
