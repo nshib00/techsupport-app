1bbcac66118abd13df1be74aa7aa0bc5f0538b08
Nikita Shibaev <nshib.54321@gmail.com>
2025-07-08 12:59:26 +0300

    Add notifications filter by is_read field

--- notifications/views.py
+++ notifications/views.py
@@ -2,7 +2,8 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from notifications.models import Notification
 from notifications.serializers import NotificationSerializer
-from drf_spectacular.utils import extend_schema_view, extend_schema
+from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter
+from django_filters import rest_framework as filters
 
 
 @extend_schema_view(
@@ -10,11 +11,21 @@ from drf_spectacular.utils import extend_schema_view, extend_schema
         summary="Cписок уведомлений пользователя",
         description="Возвращает список уведомлений, отсортированных по дате создания (по убыванию).",
         responses=NotificationSerializer(many=True),
+        parameters=[
+        OpenApiParameter(
+            name='is_read',
+            description='Прочитано/не прочитано',
+            required=False,
+            type=bool
+        )
+    ],
     )
 )
 class NotificationListView(generics.ListAPIView):
     serializer_class = NotificationSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ['is_read']
 
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user).order_by('-created_at')
